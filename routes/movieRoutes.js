dc8fd2f16cfe341cd0521cca6c367f64d4edb8d1
Lee Castelani <notayogilee@gmail.com>
2022-01-01 14:56:12 -0500

    add single movie route

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -11,6 +11,7 @@ let search = 'now_playing';
 
 let cache = apicache.middleware
 
+// get now playing movies
 router.get('/', cache('2 minutes'), async (req, res) => {
   try {
     const params = new URLSearchParams({
@@ -31,5 +32,30 @@ router.get('/', cache('2 minutes'), async (req, res) => {
     res.status(500).json({ error });
   }
 });
+
+// get single movie
+router.get('/:id', cache('2 minutes'), async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query
+    });
+
+    const apiRes = await needle('get', `${API_BASE_URL}/movie/${id}?${params}`);
+    const data = apiRes.body;
+
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`REQUEST: ${API_BASE_URL}/movie/${search}?${params}`);
+    }
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+
+})
 
 module.exports = router;
