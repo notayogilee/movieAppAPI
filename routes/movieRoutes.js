b6d00102c29e7da2fd1fd9adccf824b6331fc6ab
Lee Castelani <notayogilee@gmail.com>
2022-01-09 17:46:16 -0500

    add movie cast route

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -47,7 +47,26 @@ router.get('/:id', cache('2 minutes'), async (req, res) => {
   } catch (error) {
     res.status(500).json({ error });
   }
+});
 
-})
+// get movie cast
+router.get('/:id/credits', cache('2 minutes'), async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query
+    });
+
+    const apiRes = await needle('get', `${API_BASE_URL}/movie/${id}/credits?${params}`);
+    const data = apiRes.body;
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
 module.exports = router;
