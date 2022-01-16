0c854a96f3316a97ff138d736d1b897922d4ef3f
Lee Castelani <notayogilee@gmail.com>
2022-01-16 17:54:26 -0500

    add route for tv show cast

--- routes/showRoutes.js
+++ routes/showRoutes.js
@@ -49,4 +49,24 @@ router.get('/:id', cache('2 minutes'), async (req, res) => {
   }
 });
 
+// get show cast
+router.get('/:id/credits', cache('2 minutes'), async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query
+    });
+
+    const apiRes = await needle('get', `${API_BASE_URL}/tv/${id}/credits?${params}`);
+    const data = apiRes.body;
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
 module.exports = router;
