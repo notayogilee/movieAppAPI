cab4c0ac11e4a9333a4611e0d0ef3dff9959ce16
Lee Castelani <notayogilee@gmail.com>
2022-01-23 10:09:23 -0500

    add actor film credits, show credits and extra images routes

--- routes/actorRoutes.js
+++ routes/actorRoutes.js
@@ -49,4 +49,64 @@ router.get('/:id', cache('2 minutes'), async (req, res) => {
   }
 });
 
+// get actor movie credits
+router.get('/:id/movies', cache('2 minutes'), async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query
+    });
+
+    const apiRes = await needle('get', `${API_BASE_URL}/person/${id}/movie_credits?${params}`);
+    const data = apiRes.body;
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+// get actor tv show credits
+router.get('/:id/shows', cache('2 minutes'), async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query
+    });
+
+    const apiRes = await needle('get', `${API_BASE_URL}/person/${id}/tv_shows?${params}`);
+    const data = apiRes.body;
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+// get actor images
+router.get('/:id/images', cache('2 minutes'), async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query
+    });
+
+    const apiRes = await needle('get', `${API_BASE_URL}/person/${id}/images?${params}`);
+    const data = apiRes.body;
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
 module.exports = router;
