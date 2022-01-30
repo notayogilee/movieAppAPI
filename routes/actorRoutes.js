27a1b037de939da3fdf7e90e146a402759b4413e
Lee Castelani <notayogilee@gmail.com>
2022-01-30 18:41:14 -0500

    fix api call for actor tv show credits

--- routes/actorRoutes.js
+++ routes/actorRoutes.js
@@ -79,7 +79,7 @@ router.get('/:id/shows', cache('2 minutes'), async (req, res) => {
       ...url.parse(req.url, true).query
     });
 
-    const apiRes = await needle('get', `${API_BASE_URL}/person/${id}/tv_shows?${params}`);
+    const apiRes = await needle('get', `${API_BASE_URL}/person/${id}/tv_credits?${params}`);
     const data = apiRes.body;
 
     res.status(200).json(data);
